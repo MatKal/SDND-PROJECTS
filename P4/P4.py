e30e1079acca79e191688166ea544134cf83276d
pictonailer <GA.LTY-1997@HOTMAIL.COM>
2021-04-12 19:28:45 +0200

    Update P4.py
    Final neural network

--- P4/P4.py
+++ P4/P4.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 import numpy as np
 import csv
 import cv2
@@ -17,7 +16,7 @@ measurements = []
 for line in lines: 
     steering_center = float(line[3])
 
-    correction = 0.2 # this is a parameter to tune
+    correction = 0.1
     steering_left = steering_center + correction
     steering_right = steering_center - correction
     
@@ -35,34 +34,66 @@ for line in lines:
     images.append(img_right)
     measurements.append(steering_right)
 
+ag_images, ag_measurements = [], []
+for image, measurement in zip(images, measurements):
+    ag_images.append(image)
+    ag_measurements.append(measurement)
     
-    
-#     image_flipped = np.fliplr(image)
-#     images.append(image_flipped)
-#     measure_flipped = -measurement
-#     measurements.append(measure_flipped)
+    ag_images.append(cv2.flip(image, 1))
+    ag_measurements.append(measurement*-1.0)
 
-X_train = np.array(images)
-y_train = np.array(measurements) 
+X_train = np.array(ag_images)
+y_train = np.array(ag_measurements) 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, BatchNormalization
 from keras.layers.convolutional import Convolution2D as Conv2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
+
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Conv2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+model.add(Conv2D(24, (5, 5), strides=2, activation='elu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2, 2), padding='same'))
+
+model.add(Conv2D(36, (5, 5), strides=2, activation='elu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2, 2), padding='same'))
+
+model.add(Conv2D(48, (5, 5), strides=2, activation='elu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2, 2), padding='same'))
+
+model.add(Conv2D(64, (3, 3), activation='elu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2, 2), padding='same'))
+
+model.add(Conv2D(64, (3, 3), activation='elu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2, 2), padding='same'))
+
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(BatchNormalization())
+
+model.add(Dropout(rate=0.1))
+model.add(Dense(50))
+model.add(BatchNormalization())
+
+model.add(Dropout(rate=0.1))
+model.add(Dense(10)) 
+model.add(BatchNormalization())
+
+model.add(Dropout(rate=0.1))
 model.add(Dense(1)) 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=25)
 model.save('model.h5')
+
+print('Model Saved!')
 
 exit()
